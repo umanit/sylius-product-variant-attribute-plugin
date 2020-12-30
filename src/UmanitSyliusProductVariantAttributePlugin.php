10d6397122d74080c3aef705d47bed1258bd512f
Vivien <vdebona@umanit.fr>
2020-12-30 11:08:44 +0100

    Initial commit

---
+++ src/UmanitSyliusProductVariantAttributePlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Umanit\SyliusProductVariantAttributePlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class UmanitSyliusProductVariantAttributePlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
